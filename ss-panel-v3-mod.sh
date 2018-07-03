624f5a2dfca2fa6a0a34f86cdf7e97574bf3fe37
dumplin233 <1277385101@qq.com>
2018-07-04 02:44:04 +0800

    no message

--- ss-panel-v3-mod.sh
+++ ss-panel-v3-mod.sh
@@ -139,6 +139,11 @@ install_centos_ssr(){
 	git clone -b manyuser https://github.com/glzjin/shadowsocks.git "/root/shadowsocks"
 	cd /root/shadowsocks
 	chkconfig supervisord on
+	# 启用supervisord
+	supervisorctl shutdown
+	#某些机器没有echo_supervisord_conf 
+	wget -N -P  /etc/ --no-check-certificate  https://raw.githubusercontent.com/dumplin233/ss-panel-and-ss-py-mu/master/supervisord.conf
+	supervisord
 	#第一次安装
 	python_test
 	pip install -r requirements.txt -i $pyAddr	
@@ -188,6 +193,7 @@ install_ubuntu_ssr(){
 	# 配置程序
 	cp apiconfig.py userapiconfig.py
 	cp config.json user-config.json
+	sed -i '$a [program:ssr]\ncommand = python /root/shadowsocks/server.py\nuser = root\nautostart = true\nautorestart = true' /etc/supervisor/supervisord.conf
 }
 install_node(){
 	clear
@@ -242,11 +248,6 @@ install_node(){
 	UserNODE_ID=${UserNODE_ID:-"3"}
 	sed -i '2d' /root/shadowsocks/userapiconfig.py
 	sed -i "2a\NODE_ID = ${UserNODE_ID}" /root/shadowsocks/userapiconfig.py
-	# 启用supervisord
-	supervisorctl shutdown
-	#某些机器没有echo_supervisord_conf 
-	wget -N -P  /etc/ --no-check-certificate  https://raw.githubusercontent.com/cangshuking/ss-panel-and-ss-py-mu_cangshu/master/supervisord.conf
-	supervisord
 	#iptables
 	iptables -F
 	iptables -X  
@@ -259,9 +260,91 @@ install_node(){
 	chmod +x /etc/rc.d/rc.local
 	echo "#############################################################"
 	echo "# 安装完成，节点即将重启使配置生效                          #"
-	echo "# Github: https://github.com/cangshuking/ss-panel-and-ss-py-mu_cangshu   #"
-	echo "# Author: 91vps                                             #"
+	echo "# Github: https://github.com/mmmwhy/ss-panel-and-ss-py-mu   #"
+	echo "# Author: 91vps, dumplin                                    #"
 	echo "#############################################################"
+	reboot now
+}
+install_node_glzjin(){
+	clear
+	echo
+	echo "#############################################################"
+	echo "# One click Install Shadowsocks-Python-Manyuser             #"
+	echo "# Github: https://github.com/mmmwhy/ss-panel-and-ss-py-mu   #"
+	echo "# Author: 91vps, dumplin                                    #"
+	echo "#############################################################"
+	echo
+	#Check Root
+	[ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
+	#check OS version
+	check_sys(){
+		if [[ -f /etc/redhat-release ]]; then
+			release="centos"
+		elif cat /etc/issue | grep -q -E -i "debian"; then
+			release="debian"
+		elif cat /etc/issue | grep -q -E -i "ubuntu"; then
+			release="ubuntu"
+		elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat"; then
+			release="centos"
+		elif cat /proc/version | grep -q -E -i "debian"; then
+			release="debian"
+		elif cat /proc/version | grep -q -E -i "ubuntu"; then
+			release="ubuntu"
+		elif cat /proc/version | grep -q -E -i "centos|red hat|redhat"; then
+			release="centos"
+	  fi
+	}
+	install_ssr_for_each(){
+		check_sys
+		if [[ ${release} = "centos" ]]; then
+			install_centos_ssr
+		else
+			install_ubuntu_ssr
+		fi
+	}
+	# 取消文件数量限制
+	sed -i '$a * hard nofile 512000\n* soft nofile 512000' /etc/security/limits.conf
+	# 更改设置
+	read -p "Please input your Node_ID=: " UserNODE_ID
+	read -p "Please input your MYSQL_HOST: " MYSQL_HOST
+	read -p "Please input your MYSQL_USER: " MYSQL_USER
+	read -p "Please input your MYSQL_PASS: " MYSQL_PASS
+	read -p "Please input your MYSQL_DB: " MYSQL_DB
+	install_ssr_for_each
+	cd /root/shadowsocks
+	echo -e "modify Config.py...\n"
+	# MU_SUFFIX
+	sed -i "s#'zhaoj.in'#'jd.hk'#" /root/shadowsocks/userapiconfig.py
+	# UserNODE_ID
+	sed -i '3d' /root/shadowsocks/userapiconfig.py
+	sed -i "3i\NODE_ID = ${UserNODE_ID}" /root/shadowsocks/userapiconfig.py
+	# API_INTERFACE
+	sed -i '15d' /root/shadowsocks/userapiconfig.py
+	sed -i "15i\API_INTERFACE = 'glzjinmod'" /root/shadowsocks/userapiconfig.py
+	# MYSQL_HOST
+	sed -i '24d' /root/shadowsocks/userapiconfig.py
+	sed -i "24i\MYSQL_HOST = '${MYSQL_HOST}'" /root/shadowsocks/userapiconfig.py
+	# MYSQL_USER
+	sed -i '26d' /root/shadowsocks/userapiconfig.py
+	sed -i "26i\MYSQL_USER = '${MYSQL_USER}'" /root/shadowsocks/userapiconfig.py
+	# MYSQL_PASS
+	sed -i '27d' /root/shadowsocks/userapiconfig.py
+	sed -i "27i\MYSQL_PASS = '${MYSQL_PASS}'" /root/shadowsocks/userapiconfig.py
+	# MYSQL_DB
+	sed -i '28d' /root/shadowsocks/userapiconfig.py
+	sed -i "28i\MYSQL_DB = '${MYSQL_DB}'" /root/shadowsocks/userapiconfig.py
+	#iptables
+	iptables -F
+	iptables -X  
+	iptables -I INPUT -p tcp -m tcp --dport 22:65535 -j ACCEPT
+	iptables -I INPUT -p udp -m udp --dport 22:65535 -j ACCEPT
+	service iptables save
+	echo "#############################################################"
+	echo "# 安装完成，节点即将重启使配置生效                          #"
+	echo "# Github: https://github.com/mmmwhy/ss-panel-and-ss-py-mu   #"
+	echo "# Author: 91vps, dumplin                                    #"
+	echo "#############################################################"
+	wget --no-check-certificate https://github.com/teddysun/across/raw/master/bbr.sh && chmod +x bbr.sh && ./bbr.sh
 	reboot now
 }
 install_panel_and_node(){
@@ -269,7 +352,7 @@ install_panel_and_node(){
 	# 取消文件数量限制
 	sed -i '$a * hard nofile 512000\n* soft nofile 512000' /etc/security/limits.conf
 	install_centos_ssr
-	wget -N -P  /root/shadowsocks/ --no-check-certificate  https://raw.githubusercontent.com/cangshuking/ss-panel-and-ss-py-mu_cangshu/master/userapiconfig.py
+	wget -N -P  /root/shadowsocks/ --no-check-certificate  https://raw.githubusercontent.com/mmmwhy/ss-panel-and-ss-py-mu/master/userapiconfig.py
 	# 启用supervisord
 	echo_supervisord_conf > /etc/supervisord.conf
   sed -i '$a [program:ssr]\ncommand = python /root/shadowsocks/server.py\nuser = root\nautostart = true\nautorestart = true' /etc/supervisord.conf
@@ -292,20 +375,55 @@ install_panel_and_node(){
 	echo "# 用户名: 91vps 密码: 91vps                                  #"
 	echo "# phpmyadmin：http://${IPAddress}:888  用户名密码均为：root  #"
 	echo "# 安装完成，节点即将重启使配置生效                           #"
-	echo "# Github: https://github.com/cangshuking/ss-panel-and-ss-py-mu_cangshu    #"
+	echo "# Github: https://github.com/mmmwhy/ss-panel-and-ss-py-mu    #"
 	echo "#############################################################"
 	reboot now
 }
-echo
-echo "#############################################################"
-echo "# One click Install SS-panel and Shadowsocks-Py-Mu          #"
-echo "# Github: https://github.com/cangshuking/ss-panel-and-ss-py-mu_cangshu   #"
-echo "# Author: 91vps                                             #"
-echo "# Please choose the server you want                         #"
-echo "# 1  SS-V3_mod_panel and node One click Install             #"
-echo "# 2  SS-node One click Install                              #"
-echo "#############################################################"
-echo
+	echo
+	echo "#############################################################"
+	echo "# One click Install SS-panel and Shadowsocks-Py-Mu          #"
+	echo "# fork: https://github.com/mmmwhy/ss-panel-and-ss-py-mu     #"
+	echo "# Author: 91vps, dumplin                                    #"
+	echo "# Please choose the server you want                         #"
+	echo "# 1  SS-V3_mod_panel and node One click Install             #"
+	echo "# 2  SS-node_webapi One click Install                       #"
+	echo "# 3  SS-node_glzjin One click Install                       #"
+	echo "#############################################################"
+	echo
+ARGS=`getopt --long UserNODE_ID:,MYSQL_HOST:,MYSQL_USER:,MYSQL_PASS:,MYSQL_DB: -- "$@"`
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+while true;
+do
+    case "$1" in
+	--UserNODE_ID)  
+		UserNODE_ID=$2  
+		shift 2
+		;;
+	--MYSQL_HOST)  
+		MYSQL_HOST=$2  
+		shift 2
+		;;   
+	--MYSQL_USER)  
+		MYSQL_USER=$2  
+		shift 2
+		;;			   
+	--MYSQL_PASS)  
+    	MYSQL_PASS=$2  
+    	shift 2
+        ;;   
+	--MYSQL_DB)  
+    	MYSQL_DB=$2
+    	shift 2
+		;; 
+    --)
+        shift
+        break
+        ;;
+    *) 
+        break
+        ;;
+esac
+done
 num=$1
 if [ "${num}" == "1" ]; then
     install_panel_and_node 1
@@ -318,8 +436,11 @@ else
 		2)
 		install_node
 		;;
+		3)
+		install_node_glzjin
+		;;
 		*)
-		echo "请输入正确数字 [1-2]"
+		echo "请输入正确数字 [1-3]"
 		;;
 	esac
 fi
